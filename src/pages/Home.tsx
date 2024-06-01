99b650acc422f694240a4a530e553ed309e8e98a
suaJ <rmdwjdmind@gmail.com>
2024-06-02 00:30:56 +0900

    Design:add theme.ts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { theme } from "../styles/theme";
+import styled from "styled-components";
+
+const Title1 = styled.h1`
+  color: ${theme.typography.title1.color};
+  font-size: ${theme.typography.title1.fontSize};
+  font-weight: ${theme.typography.title1.fontWeight};
+`;
 
 function Home() {
-  return <>Mainpage</>;
+  return <Title1>세상에 이런 폰트가 나오다니 천재인듯</Title1>;
 }
 
 export default Home;
---
+++ src/styles/theme.ts
@@ -0,0 +1,117 @@
+export const theme = {
+  colors: {
+    mainColor: "#11100B",
+    mainWhite: "#FAFAFA",
+    mainBackground: "#F3EEE1",
+  },
+  typography: {
+    //web
+    title1: {
+      color: "#191F28",
+      fontSize: "48px",
+      fontWeight: 700,
+    },
+    title2: {
+      color: "#30343A",
+      fontSize: "20px",
+      fontWeight: 700,
+    },
+    subTitle: {
+      color: "#B6B0A3",
+      fontSize: "16px",
+      fontWeight: 600,
+    },
+    describtion1: {
+      color: "#191F28",
+      fontSize: "16px",
+      fontWeight: 400,
+    },
+    describtion2: {
+      color: "#191F28",
+      fontSize: "16px",
+      fontWeight: 500,
+    },
+    describtion3: {
+      color: "#191F28",
+      fontSize: "14px",
+      fontWeight: 400,
+    },
+    cardTitle1: {
+      color: "#191F28",
+      fontSize: "36px",
+      fontWeight: 700,
+    },
+    cardTitle2: {
+      color: "#191F28",
+      fontSize: "24px",
+      fontWeight: 700,
+    },
+    cardDescribtion: {
+      color: "#191F28",
+      fontSize: "20px",
+      fontWeight: 400,
+    },
+    boxSubtitle: {
+      color: "#8E8E8E",
+      fontSize: "16px",
+      fontWeight: 400,
+    },
+    boxDetail: {
+      color: "#191F28",
+      fontSize: "16px",
+      fontWeight: 500,
+    },
+    buttonText: {
+      color: "#fafafa",
+      fontSize: "20px",
+      fontWeight: 400,
+    },
+
+    //app
+    appTitle: {
+      color: "#fafafa",
+      fontSize: "26px",
+      fontWeight: 700,
+    },
+    appTitle2: {
+      color: "#191F28",
+      fontSize: "20px",
+      fontWeight: 700,
+    },
+    appSubTitle: {
+      color: "#B6B0A3",
+      fontSize: "12px",
+      fontWeight: 500,
+    },
+    appDescribtion: {
+      color: "#191F28",
+      fontSize: "14px",
+      fontWeight: 400,
+    },
+    appCardTitle: {
+      color: "#191F28",
+      fontSize: "16px",
+      fontWeight: 700,
+    },
+    appCardDescribtion: {
+      color: "#191F28",
+      fontSize: "10px",
+      fontWeight: 400,
+    },
+    appBoxTitle: {
+      color: "#191F28",
+      fontSize: "26px",
+      fontWeight: 700,
+    },
+    appBoxDetail: {
+      color: "#191F28",
+      fontSize: "14px",
+      fontWeight: 500,
+    },
+    appBoxDetail2: {
+      color: "#8E8E8E",
+      fontSize: "12px",
+      fontWeight: 400,
+    },
+  },
+};
