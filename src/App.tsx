0a8c6bc01b2fc5c9484dcfaeacfd2a2fc0dc97e5
suaJ <rmdwjdmind@gmail.com>
2024-06-01 23:17:08 +0900

    FEAT:set react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.scss";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import SearchPage from "./pages/SearchPage";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<SearchPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <>Mainpage</>;
+}
+
+export default Home;
